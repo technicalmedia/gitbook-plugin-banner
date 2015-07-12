b1adac2d5414c1ffed71048f519aa20fd6e1d0cf
Mike Graham <mgraham@technicalmedia.com>
2015-07-12 16:19:13 -0600

    updated files

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 module.exports = {
     book: {
-        //assets: "./book",
-        //js: [
-        //    "plugin.js"
-        //],
+        assets: "./book",
+        js: [
+            "plugin.js",
+            "jquery-2.1.1.min.js"
+        ],
         //css: [
         //  "styles.css"
         //],
         html: {
             "body:start": function() {
-                return "<div style='background-color: red; width: 100%; height: 75px;' />";
+                //return "<div style='background-color: red; width: 100%; height: 75px;' />";
             }
-        }
-    },
-    hooks: {
-        "page": function(page) {
-            alert('at hook');
         }
     }
 };
