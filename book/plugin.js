fd80fb4dd4cafc1611081ab2d943ebe0c4366a3a
Mike Graham <mgraham@technicalmedia.com>
2015-07-14 16:57:31 -0600

    update image size

--- book/plugin.js
+++ book/plugin.js
@@ -1,7 +1,7 @@
 require(["gitbook"], function(gitbook) {
   gitbook.events.bind("page.change", function() {
   	$(".book").prepend('<div style="position: absolute; top: 0; height: 75px; right:0; width: 300px; background-color: blue;">' 
-  		+ '<a href="http://www.cmichaelgraham.io"><img src="gitbook/plugins/gitbook-plugin-banner/cmg.jpg" /></a>' 
+  		+ '<a href="http://www.cmichaelgraham.io"><img src="gitbook/plugins/gitbook-plugin-banner/cmg.jpg" style="height: 70px; width: 70px" /></a>' 
   		+ '</div>');
   	$(".book").prepend('<div style="position: absolute; top: 75px; bottom: 0; right:0; width: 300px; background-color: lightblue;"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- responsive-ad --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1172196663696524" data-ad-slot="7874509891" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>');
   	$(".book-body").css({
