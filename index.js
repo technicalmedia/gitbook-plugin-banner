05e4de5c51e5da5607e6393406b9db7a0749ee45
Mike Graham <mgraham@technicalmedia.com>
2015-07-14 16:50:16 -0600

    add images

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ module.exports = {
         js: [
             "plugin.js",
             "jquery-2.1.1.min.js"
-        ],
+        ]
+        //,
         //css: [
         //  "styles.css"
         //],
-        html: {
-            "body:start": function() {
-                //return "<div style='background-color: red; width: 100%; height: 75px;' />";
-            }
-        }
+        //html: {
+        //    "body:start": function() {
+        //        //return "<div style='background-color: red; width: 100%; height: 75px;' />";
+        //    }
+        //}
     }
 };
