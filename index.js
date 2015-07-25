390e207340aa3caf12324f03710a1e94ff99f638
Mike Graham <mgraham@technicalmedia.com>
2015-07-25 16:35:25 -0600

    updated typescript link

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 module.exports = {
     book: {
         assets: "./book",
-        //js: [
-        //    "plugin.js",
-        //    "jquery-2.1.1.min.js"
-        //]
-        //,
         css: [
           "styles.css"
         ],
         html: {
-            "body:start": function() {
+            "body:start": function(a) {
+                // console.log(JSON.stringify(a));
                 var ret = '<div style="position: absolute; top: 0; height: 75px; right:0; width: 300px; background-color: white;">' 
-                            + '<a href="http://www.cmichaelgraham.io" target="_blank"><img src="../gitbook/plugins/gitbook-plugin-banner/cmg.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 0px" /></a>' 
-                            + '<a href="http://www.cmichaelgraham.io/building-information-systems" target="_blank"><img src="../gitbook/plugins/gitbook-plugin-banner/plotter.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 21px" /></a>' 
-                            + '<a href="http://www.cmichaelgraham.io/aurelia-typescript" target="_blank"><img src="../gitbook/plugins/gitbook-plugin-banner/aurelia.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 21px" /></a>' 
-                            + '<a href="http://www.technicalmedia.com" target="_blank"><img src="../gitbook/plugins/gitbook-plugin-banner/tm.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 21px" /></a>' 
+                            + '<a href="http://www.cmichaelgraham.io" target="_blank"><img src="' + a.staticBase + '/plugins/gitbook-plugin-banner/cmg.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 0px" /></a>' 
+                            + '<a href="http://www.cmichaelgraham.io/building-information-systems" target="_blank"><img src="' + a.staticBase + '/plugins/gitbook-plugin-banner/plotter.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 21px" /></a>' 
+                            + '<a href="http://www.cmichaelgraham.io/aurelia-typescript-samples" target="_blank"><img src="' + a.staticBase + '/plugins/gitbook-plugin-banner/aurelia.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 21px" /></a>' 
+                            + '<a href="http://www.technicalmedia.com" target="_blank"><img src="' + a.staticBase + '/plugins/gitbook-plugin-banner/tm.jpg" style="height: 59px; width: 59px; margin: 8px 0px 8px 21px" /></a>' 
                             + '</div>';
 
                 ret += '<div style="position: absolute; top: 75px; bottom: 0; right:0; width: 300px; background-color: white;"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- responsive-ad --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1172196663696524" data-ad-slot="7874509891" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>';
